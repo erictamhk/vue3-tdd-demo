04eda2b18ac0364d465380d1cf64892b0b1b1773
Eric Tam <eric.m2hk@gmail.com>
2024-09-04 18:21:30 +0800

    build sign up form

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,3 +1,20 @@
 <template>
   <h1>Sign Up</h1>
+  <div>
+    <label for="username">Username</label>
+    <input id="username" />
+  </div>
+  <div>
+    <label for="email">E-mail</label>
+    <input id="email" />
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input id="password" type="password" />
+  </div>
+  <div>
+    <label for="password-repeat">Password Repeat</label>
+    <input id="password-repeat" type="password" />
+  </div>
+  <button disabled>Sign Up</button>
 </template>
