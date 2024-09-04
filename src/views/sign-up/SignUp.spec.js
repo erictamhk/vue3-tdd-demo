5c6924a88a1a921c60888caa874959f6bd4210c3
Eric Tam <eric.m2hk@gmail.com>
2024-09-04 19:21:47 +0800

    submit to backend

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -1,6 +1,8 @@
+vi.mock('axios')
 import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import SignUp from './SignUp.vue'
+import axios from 'axios'
 
 describe('Sign Up', () => {
   it('has Sign Up header', () => {
@@ -60,6 +62,30 @@ describe('Sign Up', () => {
       await user.type(passwordInput, 'P@sswr0d')
       await user.type(passwordRepeatInput, 'P@sswr0d')
       expect(screen.getByRole('button', { name: 'Sign Up' })).toBeEnabled()
+    })
+
+    describe('when user submit form', () => {
+      it('send username, email and password to backend', async () => {
+        const user = userEvent.setup()
+        render(SignUp)
+        const usernameInput = screen.getByLabelText('Username')
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Password')
+        const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+
+        await user.type(usernameInput, 'user1')
+        await user.type(emailInput, 'user1@mail.com')
+        await user.type(passwordInput, 'P@sswr0d')
+        await user.type(passwordRepeatInput, 'P@sswr0d')
+
+        const button = screen.getByRole('button', { name: 'Sign Up' })
+        await user.click(button)
+        expect(axios.post).toHaveBeenCalledWith('/api/v1/users', {
+          username: 'user1',
+          email: 'user1@mail.com',
+          password: 'P@sswr0d'
+        })
+      })
     })
   })
 })
