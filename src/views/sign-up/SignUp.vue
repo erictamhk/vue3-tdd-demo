4b97f4fbd9cf026a63a6f840279109616cf9ae3a
Eric <eric.m2hk@gmail.com>
2024-09-05 16:55:21 +0800

    change to form submit

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,22 +1,24 @@
 <template>
   <h1>Sign Up</h1>
-  <div>
-    <label for="username">Username</label>
-    <input id="username" v-model="formState.username" />
-  </div>
-  <div>
-    <label for="email">E-mail</label>
-    <input id="email" v-model="formState.email" />
-  </div>
-  <div>
-    <label for="password">Password</label>
-    <input id="password" type="password" v-model="formState.password" />
-  </div>
-  <div>
-    <label for="password-repeat">Password Repeat</label>
-    <input id="password-repeat" type="password" v-model="formState.passwordRepeat" />
-  </div>
-  <button :disabled="isDisabled" @click="submit">Sign Up</button>
+  <form @submit.prevent="submit">
+    <div>
+      <label for="username">Username</label>
+      <input id="username" v-model="formState.username" />
+    </div>
+    <div>
+      <label for="email">E-mail</label>
+      <input id="email" v-model="formState.email" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input id="password" type="password" v-model="formState.password" />
+    </div>
+    <div>
+      <label for="password-repeat">Password Repeat</label>
+      <input id="password-repeat" type="password" v-model="formState.passwordRepeat" />
+    </div>
+    <button :disabled="isDisabled">Sign Up</button>
+  </form>
 </template>
 
 <script setup>
